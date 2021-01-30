4f3db43660a43c2b1554ef78ba5fd80a69c8ebe4
sora <sora@haeena.net>
2021-01-30 18:18:26 +0900

    update

--- include/aoip/tone.h
+++ include/aoip/tone.h
@@ -1,4 +1,21 @@
 #pragma once
 
-#define TONE_FREQ  440
+#include <math.h>
+#include <inttypes.h>
 
+#define TWO_PI (2.0f * M_PI)
+
+#define TONE_FREQ_C4 261.6f
+#define TONE_FREQ_D4 293.7f
+#define TONE_FREQ_E4 329.6f
+#define TONE_FREQ_F4 349.2f
+#define TONE_FREQ_G4 392.0f
+#define TONE_FREQ_A4 440.0f
+#define TONE_FREQ_B4 493.9f
+#define TONE_FREQ_C5 523.3f
+
+static inline float_t generate_tone_data(float_t period) {
+       return sinf(TWO_PI * period);
+}
+
+void float_to_l24(uint8_t *, float_t);
--- lib/tone.c
+++ lib/tone.c
@@ -1,1 +1,18 @@
 #include "aoip/tone.h"
+
+void float_to_l24(uint8_t l24[], float_t f) {
+	int32_t tmp_i32 = 0;
+	uint8_t *tmp_ptr = (uint8_t *)&tmp_i32;
+
+	if (f >= 1.0) {
+		tmp_i32 = 2147483647;
+	} else if (f <= -1.0) {
+		tmp_i32 = -2147483648;
+	} else {
+		tmp_i32 = floor(f * 2147483648.0);
+	}
+
+	l24[2] = tmp_ptr[0];
+	l24[1] = tmp_ptr[1];
+	l24[0] = tmp_ptr[2];
+}
